fde736a37c8d98aa81c54ce9bbed1cca75c577a2
Trist-wood <137385661+Trist-wood@users.noreply.github.com>
2023-08-15 16:20:33 +0800

    扫雷游戏

--- Minesweeper/Game.h
+++ Minesweeper/Game.h
@@ -1,1 +1,16 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS 1
+#define ROW 9
+#define COL 9
+#define ROWS ROW+2
+#define COLS COL+2
+#define EASY_COUNT 10
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+void InitBoard(char board[ROWS][COLS], int rows, int cols, char set);
+void DisplayBoard(char board[ROWS][COLS], int row, int col);
+void SetMine(char board[ROWS][COLS], int row, int col);
+void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);
